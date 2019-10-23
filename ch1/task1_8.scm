de046ed540798a1b278a034e9c51f023d62f3ccf
Vasiliy Zemlyanov <angeldaren@gmail.com>
2019-10-23 22:36:47 +0300

    Update task 1_8, use bound variables

--- ch1/task1_8.scm
+++ ch1/task1_8.scm
@@ -1,24 +1,24 @@
 (define (is-near a b)
   (< (abs (- a b)) 0.001))
 
-(define (good-enougth? guess prev-guess)
-  (is-near guess prev-guess))
-
-(define (sq a)
-  (* a a))
-
-(define (improve guess x)
-  (/ (+ (/ x (sq guess)) (* 2 guess)) 3))
-
-(define (sqrt3-iter guess prev-guess x)
-  (if (good-enougth? guess prev-guess)
-      guess
-      (sqrt3-iter (improve guess x)
-                  guess
-                  x)))
-
 (define (sqrt3 x)
-  (sqrt3-iter 1.0 0.0 x))
+
+  (define (good-enougth? guess prev-guess)
+    (is-near guess prev-guess))
+
+  (define (sq a)
+    (* a a))
+
+  (define (improve guess)
+    (/ (+ (/ x (sq guess)) (* 2 guess)) 3))
+
+  (define (sqrt3-iter guess prev-guess)
+    (if (good-enougth? guess prev-guess)
+        guess
+        (sqrt3-iter (improve guess)
+                    guess)))
+
+  (sqrt3-iter 1.0 0.0))
 
 
 (and (is-near (sqrt3 27) 3)
