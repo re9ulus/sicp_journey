3538384752cfed6e0d49f75fc2f8b3c1b6317873
Vasiliy Zemlyanov <angeldaren@gmail.com>
2019-10-26 13:51:38 +0300

    Fix pow.scm, add condition

--- ch1/code/pow.scm
+++ ch1/code/pow.scm
@@ -12,6 +12,7 @@
                    (* product b)))))
 
 (define (fast-expt b n)
+  (define (even? n) (= (remainder n 2) 0))
   (cond ((= n 0) 1)
         ((even? n) (square (fast-expt b n /2)))
         (else (* b (fast-expt b (- n 1))))))
