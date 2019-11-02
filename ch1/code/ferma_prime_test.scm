d13605a94d787996da14c47b5ad347320c4c04f7
Vasiliy Zemlyanov <angeldaren@gmail.com>
2019-11-02 12:54:07 +0300

    Add Ferma prime-test sample

---
+++ ch1/code/ferma_prime_test.scm
@@ -0,0 +1,23 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+         (else
+           (remainder (* base (expmod base (- exp 1) m))
+                      m))))
+
+(define (ferma-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((ferma-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+
+(fast-prime? 7 5) 
+(fast-prime? 4 5)
