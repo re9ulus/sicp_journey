fb166e0edd4552c2bfff3eb0d014ea77bf0660ae
Vasiliy Zemlyanov <angeldaren@gmail.com>
2019-11-22 23:11:07 +0300

    Add approx Pi computation

--- ch1/task1_31.scm
+++ ch1/task1_31.scm
@@ -4,13 +4,37 @@
       1
       (* (term a) (product term (next a) next b))))
 
+(define (inc x) (+ x 1))
+
 (define (factorial n)
   (define (id x) x)
-  (define (inc x) (+ x 1))
   (if (= n 0)
       1
       (product id 1 inc n)))
 
+(define (pi n)
+  (define (dividend x)
+    (define (trim x)
+      (if (= (remainder x 2) 0)
+          x
+          (- x 1)))
+    (+ 2 (trim x)))
+  (define (divisor x)
+    (if (= (remainder x 2) 0)
+        (- (dividend x) 1)
+        (+ (dividend x) 1)))
+  (define (term x)
+    (/ (dividend (+ x 0.0))
+       (divisor x)))
+
+  (product term 1 inc n))
+
 
 ; Test
-(= (factorial 5) 120)
+(define (near a b eps)
+  (< (abs (- a b)) eps))
+
+(and
+  (= (factorial 5) 120)
+  (near (* 4 (pi 1000)) 3.1415 0.01)
+  )
