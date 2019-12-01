3676f8bbe60738f91cbbd4ff28de227b77425e72
Vasiliy Zemlyanov <angeldaren@gmail.com>
2019-12-01 20:27:59 +0300

    Add task1_46, add fixed-point defined with iterative-improve

--- ch1/task1_46.scm
+++ ch1/task1_46.scm
@@ -14,9 +14,8 @@
 (define (near? a b)
   (< (abs (- a b)) tolerance))
 
-; sqrt redefinition
+; sqrt redefinition with iterative-improve
 (define (new-sqrt val)
-
   
   (define (average a b)
     (/ (+ a b) 2.))
@@ -30,9 +29,24 @@
   (let ((init-guess 2.0))
     ((iterative-improve ok? improve) init-guess)))
 
+; fixed-point redefinition with iterative-improve
+(define (new-fixed-point f init-guess)
+  
+  ; ok == near
+  (define (ok? guess)
+    (near? guess (f guess)))
+
+  (define (improve guess)
+    (f guess))
+
+  ((iterative-improve ok? improve) init-guess))
+
 
 ; Test
 (and 
   (near? (new-sqrt 9) 3)
   (near? (new-sqrt 2) 1.41421)
+  (near? (new-fixed-point cos 1.0) 0.739089)
+  (near? (new-fixed-point sin 1.0) 0.391432)
   )
+
