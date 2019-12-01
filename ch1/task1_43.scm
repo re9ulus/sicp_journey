14577b3c3c33eaafc790f7686343dda91368f63f
Vasiliy Zemlyanov <angeldaren@gmail.com>
2019-12-01 13:58:29 +0300

    Add task1_43, repeated general function

---
+++ ch1/task1_43.scm
@@ -0,0 +1,12 @@
+; Repeated
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= n 1)
+      f
+      (compose f (repeated f (- n 1)))))
+
+;-- Test
+(= ((repeated square 2) 5) 625)
